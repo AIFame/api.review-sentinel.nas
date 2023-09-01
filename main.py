22fb413dea707903896a334c860d77565d63adff
Nasfame <laciferin@gmail.com>
2023-09-01 06:26:47 +0530

    fixing: votes
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 1 hour 13 minutes

--- main.py
+++ main.py
@@ -1,49 +1,90 @@
 import datetime
 import os
-from collections import namedtuple
+from dataclasses import dataclass
+from pprint import pprint
 from typing import Final
 
-import requests
 import streamlit as st
 from dotenv import load_dotenv
 
-Review = namedtuple('Review', ['text', 'date', 'sentiment', 'votes'])
+load_dotenv()
+
+from clarifai import Workflow
+
+# Review = namedtuple('Review', ['text', 'date', 'sentiment', 'votes'])
 reviews = []
 
-load_dotenv()
+st.session_state.reviews = reviews
+
+
+@dataclass
+class Review:
+    text: str
+    date: str
+    sentiment: str
+    votes: int
+
 
 HUGGINGFACE_API_KEY: Final[str] = os.environ['HUGGINGFACE_API_KEY'].strip()
 
 st.title("Movie Review Sentiment Analyzer")
 new_review_text = st.text_input("Enter a movie review: ")
 if st.button("Add Review"):
-    reviews.append(Review(text=new_review_text, date=datetime.datetime.now(), sentiment=None, votes=0))
+    reviews.append(Review(text=new_review_text, date=str(datetime.datetime.now()), sentiment='', votes=0))
+
+for review in st.session_state.reviews:
+    # ic(review)
+
+    if review.sentiment is None:
+        w1 = Workflow('sentiment-analysis')
+
+        eg = [
+            {
+                "review": f"""This film stands out in today's cinema landscape. Every scene is thoughtfully crafted, and the
+        characters have depth and nuance. The story flows naturally, keeping viewers engaged. It's a
+        high point in recent movie releases.""".strip(),
+                "sentiment": 'positive'
+            }
+        ]
+
+        res = w1.run(eg[0]["review"])
+
+        outputs = res.results[0].outputs
+
+        pprint(outputs)
+
+        o = outputs[1]
+        possible_sentiments = ["Positive", "Negative", "Neutral"]
+
+        print(o)
+
+        sentiment = o.data.text.raw
+
+        for s in possible_sentiments:
+            if s.lower() in sentiment.lower():
+                sentiment = s
+                break
+
+        if sentiment not in possible_sentiments:
+            raise ValueError('invalid sentiment')
+        # sentiment = sentiment.replace("My answer:","").strip()
+
+        print(f"sentiment is {sentiment}")
+        print(f"review is {review}")
+        # Update the review's sentiment
+        # review._replace(sentiment=sentiment)
+        review.sentiment = sentiment
 
 for review in reviews:
-    if review.sentiment is None:
-        response = requests.post(
-            "https://api.huggingface.co/analyze",
-            headers={"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"},
-            json={"text": review.text}
-        )
-        result = response.json()
-        # Set thresholds for sentiment classification
-        if result['score'] > 0.05:
-            sentiment = "Positive"
-        elif result['score'] < -0.05:
-            sentiment = "Negative"
-        else:
-            sentiment = "Neutral"
-        # Update the review's sentiment
-        review = review._replace(sentiment=sentiment)
-
-for review in sorted(reviews, key=lambda r: r.votes, reverse=True):
+    print(review)
     st.write(f"Review: {review.text}")
     st.write(f"Date: {review.date}")
     st.write(f"Sentiment: {review.sentiment}")
     st.write(f"Votes: {review.votes}")
 
     if st.button("Upvote"):
-        review = review._replace(votes=review.votes + 1)
+        review.votes += 1
+        # review = review._replace(votes=review.votes + 1)
     if st.button("Downvote"):
-        review = review._replace(votes=review.votes - 1)
+        review.votes -= 1
+        # review = review._replace(votes=review.votes - 1)
